a2df59b4c79826634d18ada32adcb9dc9600ec81
AndreiBacila <dey.bacila09@yahoo.com>
2025-01-18 17:57:22 +0200

    base input focus behaviour

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello world")
+const amountInput = document.getElementById("amount");
+
+amountInput.addEventListener('focus', () => {
+    const elem = document.getElementById("amountInputContainer")
+    elem.classList.add("focused");
+})
+
+amountInput.addEventListener('blur', () => {
+    const elem = document.getElementById("amountInputContainer")
+    elem.classList.remove("focused");
+    // also validate
+})
