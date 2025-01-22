d51cfd7166ae879d094fcdb99b6808d6db56fbf2
AndreiBacila <dey.bacila09@yahoo.com>
2025-01-22 12:50:49 +0200

    solution prototype

--- script.js
+++ script.js
@@ -36,3 +36,34 @@ rateInput.addEventListener('blur', () => {
     const elem = document.getElementById("rateInputContainer")
     elem.classList.remove("focused");
 })
+
+// Submit button
+const submitButton = document.getElementById('submitButton');
+
+submitButton.addEventListener('click', function () {
+
+    if(!amountInput.checkValidity()) {
+        // invalid
+        const elem = document.getElementById("amountInputContainer")
+        elem.classList.add("error");
+        const msg = document.getElementById("amountError")
+        msg.style.display = "block";
+    } else {
+        // valid
+        const elem = document.getElementById("amountInputContainer")
+        elem.classList.remove("error");
+        const msg = document.getElementById("amountError")
+        msg.style.display = "none";
+
+        const resultsSection = document.getElementById("results")
+        resultsSection.style.display = "flex";
+        const amountResult = document.getElementById("amountResult")
+        amountResult.innerHTML = `<p>the amount input is: ${amountInput.value}</p>`
+        const previewSection = document.getElementById("preview")
+        previewSection.style.display = "none"
+    }
+
+
+  console.log(amountInput.name)
+  console.log(amountInput.value)
+});
